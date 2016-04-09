d0358788952e21a4b4aff8b232ba13811daf18ad
Sharpchain <westkk1@icloud.com>
2016-04-08 22:58:49 -0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Domini Studios HQ</title>
+		
+		<style>
+			
+			#HttpButton{
+				position: absolute;
+				text-decoration: none;
+				font-size: 18px;
+				color: white;
+				left: 400px;
+				top: 41px;
+			}
+			
+			#TutorialButton{
+				position: absolute;
+				text-decoration: none;
+				font-size: 18px;
+				color: white;
+				left: 645px;
+				top: 41px;
+			}
+			
+			#TopBar{
+			     position: absolute;
+			     background-color: rgb(255, 150, 0);
+			     height: 100px;
+			     width: 100vw;
+			     top: 0px;
+			     bottom: 0px;
+			     left: 0px;
+			     right: 0px;
+			}
+			
+			#GroupImage{
+				position: absolute;
+				left: 70px;
+			}
+			
+			#HomeButton{
+				position: absolute;
+				text-decoration: none;
+				font-size: 18px;
+				color: white;
+				left: 300px;
+				top: 41px;
+			}
+			
+			
+		
+			h2.TitleText{
+				font-size: 120%;
+				position: absolute;
+				top: 25%;
+				left: 1%;
+			}
+			
+                         p{
+                         font-size: 17px;
+                         text-align: center;
+                         color: rgb(100, 100, 100);
+                         
+                         }
+                         
+                       
+                         
+                         body{
+                         background-color: rgb(190,190, 190);
+                         background: url("http://images.virtualworldsland.com/blog/2322/796.jpg");
+                         background-repeat: no-repeat;
+                       	 background-size: cover;
+                       	 background-attachment: fixed;
+                       	 
+       
+                         }
+                       
+                         
+                        h1.TitleText{
+                        	color: rgb(0, 179, 255);
+                        	text-shadow: 1px 0px 1px black;
+                        }
+                        
+                        div.ArticleContainer{
+                        	padding: 13px;
+                        	background-color: rgb(230, 230, 230);
+                        	width: 50%;
+                        	position: relative;
+                        	left: 25%;
+                        }
+                         
+                         
+		</style>
+	</head>
+	
+	<body>
+		<div id="TopBar">
+			<h2 class="TitleText">DominiHQ</h2>
+			<a id="HomeButton" href="http://www.dominihq.freevar.com">Home</a>
+			<a id="GroupImage" href="http://www.dominihq.freevar.com">
+				<img id="GroupImage" src="http://t6.rbxcdn.com/7517cdd7da4345a495de997bcfd86a67" alt="Error Loading Image" height="100px" width="100px">
+			</a>	
+			
+			<a id="HttpButton" href="http://www.dominihq.freevar.com/HttpConfig.php">Http Service Configuration</a>
+			<a id="TutorialButton" href="http://www.dominihq.freevar.com/Tutorials.html">Tutorials</a>
+			
+		</div>
+		
+		
+		
+		<br/>	
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		
+		   <div class="ArticleContainer">
+			<h1 class="TitleText" align="center">About</h1>		
+			<p>DominiHQ is website created by the leader of Domini Studios, a group on Roblox that creates games for the Roblox Community.<br/>This website will provide Http Service communication with any of your Roblox games and will provide scripting tutorials for Roblox as well. We will also post the steps required to join Domini Studios as a Developer and the things you must agree to.<br/> If you wish to check out the group on Roblox, click on the link below.<br/>
+			
+			<a href="http://www.roblox.com/groups/group.aspx?gid=2644023">
+			Domini Studios
+			</a>
+			</p>
+		  </div>
+		  
+		  <br/>
+		  <br/>
+		  <br/>
+		  
+		  <div class="ArticleContainer">
+		  	<h1 class="TitleText" align="center">Why provide these Services?</h1>
+		  	<p>
+		  		Before I was able to make websites for myself to do the things that you can do with http service I wanted to have cross server chat<br/>But when I looked into what I needed to do to make that happened I gave up because I knew nothing about making a website. But now I do know, and since I know how difficult it can be I want to provide this service to anyone who needs it for FREE!<br/>
+		  	</p>
+		  </div>
+		  
+		  <br/>
+		  <br/>
+		  <br/>
+		  
+		  <div class="ArticleContainer">
+		  	<h1 class="TitleText" align="center">Current Developers of Domini Studios</h1>
+		  	<p>
+		  		1. sharpchain1(Leader and Scripter)<br/>
+		  		2. ThoughtConduit(Advanced Scripter)<br/>
+		  		3. iColorDev(Advanced Builder)<br/>
+		  		4. gdunn2(Builder)
+		  	</p>
+		  </div>
+			
+		<br/>
+		<br/>
+		
+	</body>
+</html>
